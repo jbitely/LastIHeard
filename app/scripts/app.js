18b1f65901743d414c094babd1b83e07056edd41
Justin Bitely <jbitely@gmail.com>
2016-02-16 18:17:41 -0500

    Implemented building chart based on json response from Last.fm api.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,36 @@
+// some mock JSON data for now
+var json = {
+  "artists" : {
+    "M83" : "27",
+    "Dance With The Dead" : "36",
+    "Carpenter Brut" : "41",
+    "Ennio Morricone" : "15",
+    "Kanye West" : "10",
+    "The Shirelles" : "7",
+    "The Mamas & the Papas" : "5",
+    "Sigur Rós" : "11",
+    "The Impressions" : "4",
+    "Buddy Holly" : "3",
+    "Dead Man's Bones" : "17",
+    "Lykke Li" : "14",
+    "The Kinks" : "14",
+    "Girl Talk" : "13",
+    "Spoon" : "12",
+    "Florence + the Machine" : "11",
+    "Sleigh Bells" : "11",
+    "Hall & Oates" : "10",
+    "Kenny Loggins" : "8",
+    "Redbone" : "8",
+    "Lana Del Rey" : "7",
+    "Pixies" : "7",
+    "Starfucker" : "7",
+    "Daniel Johnston" : "6",
+    "Kavinsky" : "6",
+    "Cyndi Lauper" : "5",
+    "Nathaniel Rateliff & the Night Sweats" : "5",
+    "Nick Cave & The Bad Seeds" : "5"
+  }
+};
 // construct httprequest - refactor option object?
 var apiurl = 'http://ws.audioscrobbler.com/2.0/';
 var apikey = '0b7b1045b71139071bd6dfa010bf3de9';
@@ -16,10 +49,13 @@ var getOptions = function(){
   // var options = $(this).serializeArray() <--- possibly use later when more options
 };
 
+var remotejson = {};
+
 var fetchData = function(requestStr){
   d3.json(requestStr, function(error, response){
-    var remotejson = response;
+    remotejson = response;
     // call our chart builder here
+    buildBubbles(remotejson.topartists.artist);
     console.log(remotejson);
   });
 };
--- app/scripts/bubbles.js
+++ app/scripts/bubbles.js
@@ -1,52 +1,47 @@
 // color scale based on colorbrewer
-var color = d3.scale.ordinal()
-  .range(colorbrewer.PuRd[9]);
+// var color = d3.scale.ordinal()
+//   .range(colorbrewer.PuRd[9]);
 
+// http://bl.ocks.org/jdarling/06019d16cb5fd6795edf
+// Adapted from http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+var randomColor = (function(){
+  var golden_ratio_conjugate = 0.618033988749895;
+  var h = Math.random();
+
+  var hslToRgb = function (h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+      r = g = b = l; // achromatic
+    }else{
+      function hue2rgb(p, q, t){
+        if(t < 0) t += 1;
+        if(t > 1) t -= 1;
+        if(t < 1/6) return p + (q - p) * 6 * t;
+        if(t < 1/2) return q;
+        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
+      }
+
+      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      var p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1/3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return '#'+Math.round(r * 255).toString(16)+Math.round(g * 255).toString(16)+Math.round(b * 255).toString(16);
+  };
+
+  return function(){
+    h += golden_ratio_conjugate;
+    h %= 1;
+    return hslToRgb(h, 0.9, 0.6);
+  };
+})();
+
+// stage setup
 var diameter = 600;
-
-
-
-// d3.json(apiurl + '?' + 'method=' + apimethod + '&user=' + username + '&format=json' + '&api_key=' + apikey, function(error, response){
-//   var remotejson = response;
-//   console.log(remotejson);
-// })
-
-// some mock JSON data for now
-var json = {
-  "artists" : {
-    "M83" : "27",
-    "Dance With The Dead" : "36",
-    "Carpenter Brut" : "41",
-    "Ennio Morricone" : "15",
-    "Kanye West" : "10",
-    "The Shirelles" : "7",
-    "The Mamas & the Papas" : "5",
-    "Sigur Rós" : "11",
-    "The Impressions" : "4",
-    "Buddy Holly" : "3",
-    "Dead Man's Bones" : "17",
-    "Lykke Li" : "14",
-    "The Kinks" : "14",
-    "Girl Talk" : "13",
-    "Spoon" : "12",
-    "Florence + the Machine" : "11",
-    "Sleigh Bells" : "11",
-    "Hall & Oates" : "10",
-    "Kenny Loggins" : "8",
-    "Redbone" : "8",
-    "Lana Del Rey" : "7",
-    "Pixies" : "7",
-    "Starfucker" : "7",
-    "Daniel Johnston" : "6",
-    "Kavinsky" : "6",
-    "Cyndi Lauper" : "5",
-    "Nathaniel Rateliff & the Night Sweats" : "5",
-    "Nick Cave & The Bad Seeds" : "5"
-  }
-};
-
-
-
 
 // viz stage
 var container = d3.select('#viz').append('svg')
@@ -63,42 +58,62 @@ var bubble = d3.layout.pack()
   .padding(3);
 
 // generate data
-var nodes = bubble.nodes(processData(json))
-  .filter(function(d){ //filter out outer bubble (any node w/o parent);
-    return d.parent;
-  });
+var buildBubbles = function(json){
+  var nodes = bubble.nodes(processData(json))
+    .filter(function(d){ //filter out outer bubble (any node w/o parent);
+      return d.parent;
+    });
 
-var vis = container.selectAll('circle')
-  .data(nodes);
+  var vis = container.selectAll('circle')
+    .data(nodes);
 
-vis.enter().append('circle')
-  .attr('transform', function(d) {return 'translate(' + d.x + ',' + d.y + ')';})
-  .attr('r', function(d) {return d.r;})
-  .attr('class', 'artist')
-  .style('fill', function(d){return color(d.size);});
+  var labels = container.selectAll('text')
+    .data(nodes);
 
-vis.enter().append('text')
-  .attr('transform', function(d) {return 'translate(' + d.x + ',' + d.y + ')';})
-  .attr('text-anchor', 'middle')
-  .attr('alignment-baseline', 'middle')
-  .style('font-size', function(d){
-    var len = d.name.substring(0, d.r / 3).length;
-    var size = d.r/4;
-    size *= 10/len;
-    size += 1;
-    return Math.round(size)+'px';
-  })
-  .text(function(d){
-    var text = d.name.substring(0, d.r /3);
-    return text;
-  });
+  vis.enter().append('circle')
+    .attr('transform', function(d) {return 'translate(' + d.x + ',' + d.y + ')';})
+    .attr('r', function(d) {return d.r;})
+    .attr('class', 'artist')
+    .style('fill', 'white');
+
+  labels.enter().append('text')
+    .attr('transform', function(d) {return 'translate(' + d.x + ',' + d.y + ')';})
+    .attr('text-anchor', 'middle')
+    .attr('alignment-baseline', 'middle')
+    .style('font-size', function(d){
+      var len = d.name.substring(0, d.r / 3).length;
+      var size = d.r/4;
+      size *= 10/len;
+      size += 1;
+      return Math.round(size)+'px';
+    })
+    .text(function(d){
+      var text = d.name.substring(0, d.r /3);
+      return text;
+    });
+
+  vis.transition().duration(1000)
+    .attr('transform', function(d) {return 'translate(' + d.x + ',' + d.y + ')';})
+    .attr('r', function(d) {return d.r;})
+    .style('fill', randomColor);
+
+  labels.transition().duration(1000)
+    .attr('transform', function(d) {return 'translate(' + d.x + ',' + d.y + ')';})
+    .attr('r', function(d) {return d.r;})
+    .text(function(d){
+      var text = d.name.substring(0, d.r /3);
+      return text;
+    });
+
+  vis.exit().remove();
+  labels.exit().remove();
+};
 
 // build data into children array
 function processData(data) {
-  var obj = data.artists;
   var newDataSet = [];
-  for(var prop in obj) {
-    newDataSet.push({name: prop, size: obj[prop]});
+  for(var i = 0; i < data.length; i++) {
+    newDataSet.push({name: data[i].name, size: data[i].playcount});
   }
   return {children: newDataSet};
 }
